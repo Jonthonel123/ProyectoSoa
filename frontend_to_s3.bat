48f4261427697fdcfa092126b91db547953a9725
Jonthonel <jcmrm01@hotmail.com>
2020-02-21 11:57:52 -0500

    cambios en el S3

--- frontend_to_s3.bat
+++ frontend_to_s3.bat
@@ -1,1 +1,1 @@
-aws s3 cp --recursive ./appweb/s3/pe_soapro_ripley/ s3://pe-soapros-capacniam-bucket-frontend-dev/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp --recursive ./public/ s3://pe-soapros-bugtracker-fe --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
