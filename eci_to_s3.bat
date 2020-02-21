75e3325e9096700378f994a74e9f75d198ee3e17
Jonthonel <jcmrm01@hotmail.com>
2020-02-21 11:55:28 -0500

    para los cambios en el S3

---
+++ eci_to_s3.bat
@@ -0,0 +1,1 @@
+aws s3 cp --recursive ./assets/ s3://pe-soapros-bugtracker-fe --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
---
+++ frontend_to_s3.bat
@@ -0,0 +1,1 @@
+aws s3 cp --recursive ./appweb/s3/pe_soapro_ripley/ s3://pe-soapros-capacniam-bucket-frontend-dev/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
